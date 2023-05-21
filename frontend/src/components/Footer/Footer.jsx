2b9ce89f938d0a0b35525e82c63cf883e61db6c2
Behram035 <behramkh035@gmail.com>
2023-05-21 17:08:52 +0500

    Add code t Github

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <p className={styles.footer}>&copy; EventGuru Management Team 2023</p>
+    )
+}
+
+export default Footer;
